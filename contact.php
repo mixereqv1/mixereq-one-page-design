bdbdc0978058df03d4fd0aee07ac80c97d64a1c4
mixereqv1 <mateuszral7@gmail.com>
2019-05-07 15:12:26 +0200

    Contact corretion...

--- contact.php
+++ contact.php
@@ -2,7 +2,9 @@
     $to_mail = 'contact@mixereq.pl';
     $imie = $_POST['firstname'];
     $subject = 'Wiadomość ze strony mixereq.pl od ' .$imie;
-    $headers = 'From: ' .$_POST['email'] 'Content-Type: text/html;charset=utf-8';
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+    $headers .= 'From: '.$_POST['email'].''."\r\n";
     $message = $_POST['message'];
     mail($to_mail,$subject,$message,$headers);
     header('location:index.html');
